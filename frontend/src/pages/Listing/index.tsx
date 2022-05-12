d0fc0dec06fa0c56ab1b442b4c7f29eda352fc8f
Ruan thiago <ruanthiago.in@gmail.com>
2022-05-11 23:30:03 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,41 @@
+
 import Pagination from "components/Pagination";
 
-function Listing(){
+import MovieCard from "components/MovieCard";
 
-        return(
+function Listing() {
+
+    return (
+
+        <>
             <Pagination />
-        );
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    
+                    
+
+                </div>
+            </div>
+
+
+        </>
+
+    );
 
 }
 export default Listing;
