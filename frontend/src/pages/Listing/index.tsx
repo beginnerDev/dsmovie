e7cbcd048f5abfe0e57c31e4eedf3bef2a61b216
Ruan thiago <ruanthiago.in@gmail.com>
2022-05-15 00:13:17 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,6 +2,14 @@
 import Pagination from "components/Pagination";
 
 import MovieCard from "components/MovieCard";
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
+
+
+        axios.get(`${BASE_URL}/movies?size+12$page=0`)
+        .then(response =>{
+            console.log(response.data);
+        })
 
 function Listing() {
 
